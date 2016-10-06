80646806e851088cf0fca39a0ae174d889d037b3
Pallandor <roger.sejas@gmail.com>
2016-10-06 23:35:51 +1100

    Add additional to TodoView Component

--- client/components/TodoView/index.js
+++ client/components/TodoView/index.js
@@ -1,14 +1,40 @@
 // for stub purposes
-import React from 'react';
+import React, { Component } from 'react';
 
-const TodoView  = ({ todo }) => (
-  <div>
-    <p>Todo: {todo}</p>
-  </div>
-);
+class TodoView extends Component {
+  constructor(){
+    super();
+    this.handleTodoRename = this.handleTodoRename.bind(this);
+    this.toggleTodoCompleted = this.toggleTodoCompleted.bind(this);
+  }
+
+  handleTodoRename(){
+    const todo = this.props.todo;
+    todo.task = prompt('Task name', todo.task) || todo.task;
+  }
+
+  toggleTodoCompleted(){
+    const todo = this.props.todo;
+    todo.completed = !todo.completed;
+  }
+
+  render() {
+    const todo = this.props.todo;
+    return (
+      <li onDoubleClick={this.handleTodoRename}>
+        <input
+          type='checkbox'
+          checked={todo.completed}
+          onChange={this.toggleTodoCompleted}
+        />
+        {todo.task}
+        {todo.assignee ? <small>{todo.assignee.name}</small> : null}
+      </li>
+    );
+}
 
 TodoView.propTypes = {
-  todo: React.PropTypes.any, // NOTE: Change this, too lax!
+  todo: React.PropTypes.object, // NOTE: Change this, too lax! or back to .any
 };
 
 export default TodoView;
